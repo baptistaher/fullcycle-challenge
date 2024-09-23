5f67bfb25c7dda1cf0c3e13ad92aad9842fad13c
Bosshentai <baptistamhernani@gmail.com>
2024-09-23 14:06:30 -0100

    finish the Clean Arquitecture class . And only missing challenge to be done

--- clean-architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ clean-architecture/src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -37,4 +37,76 @@ describe("E2E test for customer", () => {
 
     expect(response.status).toBe(500);
   });
+
+  it("should list all customer", async () => {
+    const response1 = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street 1",
+          city: "City 1",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response1.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 123,
+          zip: "12343",
+        },
+      });
+
+    expect(response2.status).toBe(200);
+
+    const listResponse = await request(app).get("/customer").send();
+
+    expect(listResponse.status).toBe(200);
+    expect(listResponse.body.customers.length).toBe(2);
+    const customer = listResponse.body.customers[0];
+    expect(customer.name).toBe("John");
+    expect(customer.address.street).toBe("Street 1");
+    expect(customer.address.city).toBe("City 1");
+    expect(customer.address.number).toBe(123);
+    expect(customer.address.zip).toBe("12345");
+
+    const customer2 = listResponse.body.customers[1];
+    expect(customer2.name).toBe("Jane");
+    expect(customer2.address.street).toBe("Street 2");
+    expect(customer2.address.city).toBe("City 2");
+    expect(customer2.address.number).toBe(123);
+    expect(customer2.address.zip).toBe("12343");
+
+    const listResponseXML = await request(app)
+      .get("/customer")
+      .set("Accept", "application/xml")
+      .send();
+
+    expect(listResponseXML.status).toBe(200);
+    expect(listResponseXML.text).toContain(
+      '<?xml version="1.0" encoding="UTF-8"?>'
+    );
+    expect(listResponseXML.text).toContain("<customers>");
+    expect(listResponseXML.text).toContain("<customer>");
+    expect(listResponseXML.text).toContain("<name>John</name>");
+    expect(listResponseXML.text).toContain("<street>Street 1</street>");
+    expect(listResponseXML.text).toContain("<city>City 1</city>");
+    expect(listResponseXML.text).toContain("<number>123</number>");
+    expect(listResponseXML.text).toContain("</customer>");
+    expect(listResponseXML.text).toContain("<customer>");
+    expect(listResponseXML.text).toContain("<name>Jane</name>");
+    expect(listResponseXML.text).toContain("<street>Street 2</street>");
+    expect(listResponseXML.text).toContain("<city>City 2</city>");
+    expect(listResponseXML.text).toContain("<number>123</number>");
+    expect(listResponseXML.text).toContain("</customer>");
+    expect(listResponseXML.text).toContain("</customers>");
+  });
 });
--- clean-architecture/src/infrastructure/api/routes/customer.route.ts
+++ clean-architecture/src/infrastructure/api/routes/customer.route.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import CustomerPresenter from "../presenters/customer.presenter";
 export const customerRoute = express.Router();
 
 customerRoute.post("/", async (req: Request, res, Response) => {
@@ -22,4 +24,20 @@ customerRoute.post("/", async (req: Request, res, Response) => {
   } catch (error) {
     res.status(500).send(error);
   }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const useCase = new ListCustomerUseCase(new CustomerRepository());
+  const output = await useCase.execute({});
+
+  res.format({
+    json: async () => res.send(output),
+    xml: async () => res.send(CustomerPresenter.listXML(output)),
+  });
+
+  // try {
+  //   res.send(output);
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
 });
