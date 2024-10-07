2e8df82c2a9e50774380e2f35c2235482db2a202
Bosshentai <baptistamhernani@gmail.com>
2024-10-07 20:43:22 -0100

    to be continue implements findAll in ProductRepository

--- monolithic/src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ monolithic/src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -49,4 +49,35 @@ describe("ProductAdmFacade test", () => {
     expect(product.purchasePrice).toBe(input.purchasePrice);
     expect(product.stock).toBe(input.stock);
   });
+
+  it("should find a product stock", async () => {
+    const productFacade = ProductAdmFacadeFactory.create();
+
+    const input = {
+      id: "1",
+      name: "Product 1",
+      description: "Product 1 description",
+      purchasePrice: 100,
+      stock: 10,
+    };
+    await productFacade.addProduct(input);
+
+    const result = await productFacade.checkStock({ productId: "1" });
+
+    expect(result.productId).toBe(input.id);
+    expect(result.stock).toBe(input.stock);
+
+    // const product = await ProductModel.findOne({
+    //   where: {
+    //     id: "1",
+    //   },
+    // });
+
+    // expect(product).toBeDefined();
+    // expect(product.id).toBe(input.id);
+    // expect(product.name).toBe(input.name);
+    // expect(product.description).toBe(input.description);
+    // expect(product.purchasePrice).toBe(input.purchasePrice);
+    // expect(product.stock).toBe(input.stock);
+  });
 });
---
+++ monolithic/src/modules/product-adm/usecase/check-stock/check-stock.usecase.spec.ts
@@ -0,0 +1,33 @@
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
+import CheckStockUseCase from "./check-stock.usecase";
+const product = new Product({
+  id: new Id("1"),
+  name: "Product 1",
+  description: "Product 1 description",
+  purchasePrice: 100,
+  stock: 10,
+});
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+  };
+};
+
+describe("CheckStock use-case unit test", () => {
+  it("should get stock of a product", async () => {
+    const ProductRepository = MockRepository();
+    const checkStockUsecase = new CheckStockUseCase(ProductRepository);
+    const input = {
+      productId: "1",
+    };
+
+    const result = await checkStockUsecase.execute(input);
+
+    expect(ProductRepository.find).toHaveBeenCalled();
+    expect(result.productId).toBe(product.id.id);
+    expect(result.stock).toBe(product.stock);
+  });
+});
