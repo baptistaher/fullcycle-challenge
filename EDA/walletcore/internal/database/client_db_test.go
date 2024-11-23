c21a0d1d8e31acb569fe6b9be70523b0468eb615
Bosshentai <baptistamhernani@gmail.com>
2024-11-23 18:29:25 -0100

    Start createing the migrations

--- EDA/walletcore/internal/database/client_db_test.go
+++ EDA/walletcore/internal/database/client_db_test.go
@@ -20,7 +20,7 @@ func (s *ClientDBTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.db = db
-	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at datetime, updated_at datetime)")
+	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at timestamp, updated_at timestamp)")
 	s.clientDB = NewClientDB(db)
 }
 
