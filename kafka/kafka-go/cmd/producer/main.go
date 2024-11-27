33a352203b5db309c6f8b6d75bbad943957bd1cd
Bosshentai <baptistamhernani@gmail.com>
2024-11-27 17:16:59 -0100

    Start the projec wit kafka connect

--- kafka/kafka-go/cmd/producer/main.go
+++ kafka/kafka-go/cmd/producer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -8,17 +9,35 @@ import (
 
 func main() {
 
+	deliveryChannel := make(chan kafka.Event)
+
 	producer := NewKafkaProducer()
 
-	Publish("messaege", "teste", producer, nil)
+	Publish("messaege", "teste", producer, []byte("transferencia"), deliveryChannel)
+	go DeliveryReport(deliveryChannel) // async
+
+	fmt.Println("Hello World")
 	producer.Flush(5000)
 
+	// sync
+	// e := <-deliveryChannel
+
+	// msg := e.(*kafka.Message)
+	// if msg.TopicPartition.Error != nil {
+	// 	fmt.Println("Erro ao enviar")
+	// } else {
+	// 	fmt.Println("Mensagem enviada", msg.TopicPartition)
+	// }
+	// producer.Flush(1000)
 	// fmt.Println("Hello Go")
 }
 
 func NewKafkaProducer() *kafka.Producer {
 	configMap := &kafka.ConfigMap{
-		"bootstrap.servers": "kafka-go-kafka-1",
+		"bootstrap.servers":   "kafka-go-kafka-1:9092",
+		"delivery.timeout.ms": "0",
+		"acks":                "all",  // 0 , 1 or all
+		"enable.idempotence":  "true", // default false
 	}
 
 	producer, err := kafka.NewProducer(configMap)
@@ -30,7 +49,7 @@ func NewKafkaProducer() *kafka.Producer {
 
 }
 
-func Publish(msg string, topic string, producer *kafka.Producer, key []byte) error {
+func Publish(msg string, topic string, producer *kafka.Producer, key []byte, deliveryChannel chan kafka.Event) error {
 	message := &kafka.Message{
 		Value: []byte(msg),
 		TopicPartition: kafka.TopicPartition{
@@ -40,9 +59,26 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte) err
 		Key: key,
 	}
 
-	err := producer.Produce(message, nil)
+	err := producer.Produce(message, deliveryChannel)
 	if err != nil {
 		log.Println(err.Error())
+		return err
 	}
+
 	return err
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+	for e := range deliveryChan {
+		switch ev := e.(type) {
+		case *kafka.Message:
+			if ev.TopicPartition.Error != nil {
+				fmt.Println("Erro ao enviar")
+			} else {
+				fmt.Println("Mensagem enviada", ev.TopicPartition)
+				// save in the database de message was process
+				// ex: confirm a bank transaction was done.
+			}
+		}
+	}
 }
