73e0d28311e2049d955a784d7b9a58efd3b4b8d5
Bosshentai <baptistamhernani@gmail.com>
2024-11-29 12:38:29 -0100

    The configuration have been change for the location for the main

--- EDA/wallet_web_app/src/app/main/App.tsx
+++ EDA/wallet_web_app/src/app/main/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -12,20 +12,20 @@ function App() {
         <a
           href="https://vite.dev"
           target="_blank">
-          <img
+          {/* <img
             src={viteLogo}
             className="logo"
             alt="Vite logo"
-          />
+          /> */}
         </a>
         <a
           href="https://react.dev"
           target="_blank">
-          <img
+          {/* <img
             src={reactLogo}
             className="logo react"
             alt="React logo"
-          />
+          /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
