a7b52fa26543f808456a3e57876007aa88db73b4
Bosshentai <baptistamhernani@gmail.com>
2024-12-05 10:38:53 -0100

    Start fix the problem with the route callback

--- keycloack/authentication-flow/authorization-code-flow/src/index.ts
+++ keycloack/authentication-flow/authorization-code-flow/src/index.ts
@@ -10,13 +10,45 @@ app.get("/login", (req, res) => {
     scope: "openid",
   });
 
-  const url = `https://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`;
+  const url = `http://localhost:8080/realms/fullcycle-realm/protocol/openid-connect/auth?${loginParams.toString()}`;
 
   console.log(url);
   res.redirect(url);
-  // res.send("hello world");
+});
+
+app.get("/callback", async (req, res) => {
+  //here
+
+  console.log(req.query);
+
+  const bodyParams = new URLSearchParams({
+    client_id: "fullcycle-client",
+    grant_type: "authorization_code",
+    code: req.query.code as string,
+    redirct_uri: "http://localhost:3000/callback",
+  });
+
+  const url =
+    "http://host.docker.internal:8080/realms/fullcycle-realm/protocol/openid-connect/token";
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: bodyParams.toString(),
+  });
+
+  console.log(response);
+
+  const result = await response.json();
+
+  console.log(result);
+
+  res.json(result);
+  // res.send("ok");
 });
 
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log("Listening on port 3000");
 });
