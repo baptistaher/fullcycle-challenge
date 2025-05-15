e523d16f7e3f2a498ef47dc3ec96b8ab7dff3daf
Bosshentai <baptistamhernani@gmail.com>
2025-05-15 09:14:31 -0100

    feat: add observabily with elastic search

---
+++ docker/FC4/mygo_app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	args :=
+		os.Args[1:]
+
+	if len(args) > 0 {
+		fmt.Printf("Starting server at port %s\n", args[0])
+		http.ListenAndServe(":"+args[0], nil)
+	}
+
+	fmt.Println("Starting server at port 8080")
+	http.ListenAndServe(":8080", nil)
+}
